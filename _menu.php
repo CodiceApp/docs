936cede69f2007d623d3d938ac7382a0e5710f6f
Sobak <msobaczewski@gmail.com>
2017-01-07 23:19:27 +0100

    Menu and images are now stored alongside the docs for easier maintanance and versioning

---
+++ _menu.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    ['header', 'Getting started'],
+    ['installation', 'Installation'],
+    ['contributing', 'Contributing'],
+
+    ['header', 'User guide'],
+    ['basics', 'Basic concepts'],
+    ['calendar', 'Calendar'],
+    ['labels', 'Labels'],
+
+    ['header', 'Plugins'],
+    ['plugin-introduction', 'Introduction'],
+    ['plugin-actions', 'Actions'],
+    ['plugin-filters', 'Filters'],
+
+    ['header', 'Appendices'],
+    ['actions-list', 'Actions index'],
+    ['filters-list', 'Filters index'],
+    ['https://github.com/Sobak/Codice/releases', 'Changelog'],
+];
